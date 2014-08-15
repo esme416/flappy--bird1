cce2b8f35146abd016c552456b1e38923bd57c26
sotelo.esmeralda16@gmail.com <>
2014-08-15 15:41:37 +0000

    flappy boxxn

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ var mainState = {
    
    game.load.image ('bird','assets/bird.png');
    
-   game.load.image('pipe','assets/bird.png');
+   game.load.image('pipe','assets/pipe.png');
    
    },
     
@@ -43,6 +43,20 @@ var mainState = {
    var spaceKey = this.game.input.keyboard.addKey (Phaser.Keyboard.SPACEBAR);
    
    spaceKey.onDown.add(this.jump, this);
+   
+  this.pipes = game.add.group();
+  
+  this.pipes.enableBody=  true ;
+  
+  this.pipes.createMultiple (20, 'pipe');
+  
+  this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+   
+this.score = 0
+this.labelScore = game.add.text(20,20, "0", {font: "30px Arial", fill: "#ffffff"}); 
+
+
+   
    }, 
     update: function () {
      //This function runs 60 times per second
@@ -53,9 +67,39 @@ var mainState = {
        this.restartGame();
        
      }
-    
+     
+     game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+   
      },
-    
+     addOnePipe: function (x,y) {
+       //Get the first dead pipe in our group
+       var pipe = this.pipes.getFirstDead ();
+       
+       //Set the new position
+       pipe.reset(x,y)
+       
+       pipe.body.velocity.x = -200;
+       pipe.checkWorldBounds = true;
+       pipe.outOfBoundsKill = true; 
+  
+       },
+      addRowOfPipes: function() {   
+      var hole = Math.floor(Math.random()* 5) + 1;
+      
+      for(var i = 0; i < 8; i++)
+        if(i != hole && i != hole + 1){
+          
+          this.addOnePipe(400, i *60 +10); 
+          
+        }
+        
+        this.score += 1; 
+        this.labelScore.text = this.score; 
+      
+    },
+     
+     
+     
     jump: function() {
       //Let's make our bird jump!
       this.bird.body.velocity.y = -355; 
